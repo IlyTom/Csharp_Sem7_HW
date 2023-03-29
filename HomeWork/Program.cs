f7dda67ff7de0e2ffb890d7da4e9d5a09b4efcf2
IlyTom <ilytoma@mail.ru>
2023-03-29 21:31:26 +0300

    2 задача

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,21 +1,34 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-// m = 3, n = 4.
-// 0,5 7 -2 -0,2
-// 1 -3,3 8 -9,9
-// 8 7,8 -7,1 9
-
-double[,] FillArray(int m, int n, int minValue, int maxValue)
+﻿// Задача 50. Напишите программу, которая на вход принимает два числа(строка, столбец), 
+// проверяя есть ли такая позиция в двумерном массиве и возвращает сообщение о том, 
+// что оно найдено, а также какое число стоит на этом месте или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// i = 1, j = 3 -> Такой элемент есть: 3
+// i = 4, j = 2 -> такого элемента в массиве нет
+(int m, int n) ArraySize()
 {
-    double[,] Array = new double[m, n];
+    System.Console.Write("Введите кол-во строк: ");
+    int m = int.Parse(Console.ReadLine()!);
+    System.Console.Write("Введите кол-во столбцов: ");
+    int n = int.Parse(Console.ReadLine()!);
+    var result = (m, n);
+    return result;
+}
+
+int[,] FillArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] Array = new int[m, n];
     for (int i = 0; i < Array.GetLength(0); i++)
         for (int j = 0; j < Array.GetLength(1); j++)
         {
-            Array[i, j] = Math.Round(new Random().NextDouble() + new Random().Next(minValue, maxValue + 1), 1);
+            Array[i, j] = new Random().Next(minValue, maxValue + 1);
         }
     return Array;
 }
 
-void PrintArray(double[,] arr)
+void PrintArray(int[,] arr)
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
@@ -27,5 +40,22 @@ void PrintArray(double[,] arr)
     }
 }
 
-double[,] Array = FillArray(3, 4, -99, 99);
+void CheckPosition(int[,] arr)
+{
+    System.Console.Write("Введите номер строки: ");
+    int i = int.Parse(Console.ReadLine()!);
+    System.Console.Write("Введите номер столбца: ");
+    int j = int.Parse(Console.ReadLine()!);
+    if (i < arr.GetLength(0) && j < arr.GetLength(1)) System.Console.WriteLine($"i = {i}, j = {j} -> Такой элемент есть: {arr[i, j]}");
+    else System.Console.WriteLine($"i = {i}, j = {j} -> Такого элемента в массиве нет");
+}
+
+
+var ArrSize = ArraySize();
+int m = ArrSize.m;
+int n = ArrSize.n;
+
+int[,] Array = FillArray(m, n, 0, 8);
 PrintArray(Array);
+CheckPosition(Array);
+
